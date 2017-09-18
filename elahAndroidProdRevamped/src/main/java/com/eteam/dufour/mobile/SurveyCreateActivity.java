2c4db97d0a899c18bd006d2430399372ce2237dd
Jinu Jayakumar <jinuj27@gmail.com>
2017-09-18 17:50:05 +0530

    added google repo and updated build gradle

--- elahAndroidProdRevamped/src/main/java/com/eteam/dufour/mobile/SurveyCreateActivity.java
+++ elahAndroidProdRevamped/src/main/java/com/eteam/dufour/mobile/SurveyCreateActivity.java
@@ -1,13 +1,5 @@
 package com.eteam.dufour.mobile;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -38,26 +30,34 @@ import com.eteam.utils.Consts;
 import com.eteam.utils.ElahHttpClient;
 import com.eteam.utils.Util;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class SurveyCreateActivity extends Activity implements OnClickListener{
 	// ===========================================================
 	// Constants
 	// ===========================================================
 	private static final String INTENT_CUSTOMER_NAME = "com.eteam.dufour.release.backup.SurveyButtonActivity.INTENT_CUSTOMER_NAME";
 	private static final String INTENT_CUSTOMER_CODE = "com.eteam.dufour.release.backup.SurveyButtonActivity.INTENT_CUSTOMER_CODE";
-	
+
 	private static final String KEY_REQUEST = "reqfor";
 	private static final String KEY_CUST_CODE = "cust_code";
 	private static final String KEY_CURRENT_TIME = "ct";
 //	private static final String KEY_SALESPERSON_CODE = "sales_code";
-	
+
 	private static final String VALUE_SURVEY_CHECK = "previousSurveyCheck";
 	private static final String VALUE_SURVEY_GET_PREVIOUS = "previousSurvey";
-	
-	
+
+
 	// ===========================================================
 	// Fields
 	// ===========================================================
-	
+
 	private View mLayoutBtn;
 	private View mBtnCopySurvey;
 	private View mBtnErrorCopying;
@@ -66,29 +66,29 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 	private View mBtnCancel;
 	private View mBtnDrafts;
 	private TextView mLblNetNotAvailable;
-	
+
 	private String customerName;
 	private String customerCode;
 	private String salesPersonCode;
-	
+
 	private Toast mToast;
-		
+
 	private ElahDBHelper dbHelper;
-	
+
 	private PreviousSurveyCheckTask mCheckTask;
 	private GetPreviousSurveyTask mCopyTask;
 
-	
+
 	private SharedPreferences mPrefs;
-	
+
 	private NetConnectionReceiver mReceiver;
-	
+
 	private long draftId;
-	
+
 	// ===========================================================
 	// Constructors
 	// ===========================================================
-	
+
 	// ===========================================================
 	// Launchers
 	// ===========================================================
@@ -99,7 +99,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		intent.putExtra(INTENT_CUSTOMER_CODE, customerCode);
 		activity.startActivity(intent);
 	}
-	
+
 	public static final void startActivityForResult(Activity activity
 			,String customerName,String customerCode,int callingActivity){
 		Intent intent = new Intent(activity,SurveyCreateActivity.class);
@@ -121,12 +121,12 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.survey_btn_layout);
 		setFromIntent(savedInstanceState);
-		
+
 		mToast = Toast.makeText(this, "Sample", Toast.LENGTH_SHORT);
-		
+
 		TextView fieldPDVCode = (TextView) findViewById(R.id.field_pdv);
 		fieldPDVCode.setText(Html.fromHtml("<b>"+getResources().getString(R.string.pdv)+"</b>: "+customerCode));
-		
+
 		mLayoutBtn = findViewById(R.id.layout_btn);
 		mBtnCopySurvey = findViewById(R.id.btn_copia_valori);
 		mBtnErrorCopying = findViewById(R.id.btn_error_copying);
@@ -135,24 +135,29 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		mBtnCancel = findViewById(R.id.btn_annulla);
 		mBtnDrafts = findViewById(R.id.btn_riprendi_la_bozza_esistente);
 
-		
+
 		mLblNetNotAvailable = (TextView) findViewById(R.id.lbl_msg_no_net);
-		
-		
+
+
 		mBtnCopySurvey.setOnClickListener(this);
 		mBtnNewSurvey.setOnClickListener(this);
 		mBtnCancel.setOnClickListener(this);
 		mBtnDrafts.setOnClickListener(this);
 		mBtnErrorCopying.setOnClickListener(this);
-		
+
 		mPrefs = getSharedPreferences(Consts.PREF_ELAH, MODE_PRIVATE);
 		salesPersonCode = mPrefs.getString(LoginActivity.PREF_SALESPERSON, "");
-		
+
 		dbHelper = ElahDBHelper.getInstance(this);
 		setUpDraftsButtons();
 		setUpCopyButton();
-	}
-	
+        try {
+            LoginActivity.copyAppDbToDownloadFolder(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 	@Override
@@ -161,7 +166,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		super.onStart();
 		setUpReceiver();
 	}
-	
+
 
 
 	@Override
@@ -169,30 +174,30 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onResume();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 	}
-	
-	
-	
+
+
+
 	@Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
 		unregisterReceiver(mReceiver);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-	
-	
-	
+
+
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
@@ -201,7 +206,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		super.onSaveInstanceState(outState);
 
 	}
-	
+
 	public void onClick(View v) {
 		if(v==mBtnCopySurvey){
 			if(mCopyTask==null){
@@ -217,7 +222,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 //			finish();
 		}
 		else if(v==mBtnNewSurvey){
-			
+
 			long surveyId = TableSurvey.createNewSurvey(new Survey(salesPersonCode, customerCode, Util.getCurrentDateTime(),1,0));
 			SurveyTabActivity.startActivity(this, surveyId, customerName, customerCode);
 			setResult(RESULT_OK);
@@ -236,12 +241,12 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		else if(v==mBtnErrorCopying){
 			startSurveyCheckActivity();
 		}
-		
+
 	}
 	// ===========================================================
 	// Methods
 	// ===========================================================
-	
+
 	private void setFromIntent(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		if(savedInstanceState!=null){
@@ -253,7 +258,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			customerName = getIntent().getStringExtra(INTENT_CUSTOMER_NAME);
 		}
 	}
-	
+
 	private void startSurveyCheckActivity(){
 		if(mCheckTask==null){
 			mCheckTask = new PreviousSurveyCheckTask(this);
@@ -266,14 +271,14 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			}
 		}
 	}
-	
+
 	private void setUpReceiver() {
 		// TODO Auto-generated method stub
 		mReceiver = new NetConnectionReceiver();
 		IntentFilter mFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
 		registerReceiver(mReceiver, mFilter);
 	}
-	
+
 	private void setUpCopyButton() {
 		// TODO Auto-generated method stub
 		if(Util.haveNetworkConnection(this)){
@@ -294,9 +299,9 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 				mBtnErrorCopying.setVisibility(View.GONE);
 		}
 	}
-	
+
 	private void setUpDraftsButtons() {
-		
+
 		draftId = TableSurvey.getDraftFor(customerCode);
 		if(draftId!=-1){
 			mBtnDrafts.setVisibility(View.VISIBLE);
@@ -305,7 +310,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			mBtnDrafts.setVisibility(View.GONE);
 		}
 	}
-	
+
 	public void startSurveyTabActivity(Long surveyId) {
 		SurveyTabActivity.startActivity(SurveyCreateActivity.this, surveyId
 				, customerName, customerCode);
@@ -316,49 +321,49 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
-	
+
 	private class PreviousSurveyCheckTask extends AsyncTask<String,Integer, String>{
-		
+
 		private final static String RESPONSE_SURVEY_EXISTS = "1";
 		private final static String RESPONSE_SURVEY_EXPIRED = "2";
 		private final static String KEY_RESPONSE = "S";
 		private static final String LOGIN_FAILED = "lf";
 		private static final String PERMISSION_NOT_ALLOWED = "pna";
 		private static final String LOGIN_SERVER_ERROR = "lse";
-		
+
 		private ElahProgress mProgress;
 		private Context 	   mContext;
-		
+
 		// ===========================================================
 		// Constructors
 		// ===========================================================
-		
+
 		private PreviousSurveyCheckTask(Context context){
 			this.mContext = context;
 		}
-		
+
 		// ===========================================================
 		// Getter & Setter
 		// ===========================================================
-		
+
 		// ===========================================================
 		// Methods for/from SuperClass/Interfaces
 		// ===========================================================
-		
+
 		@Override
 		protected void onPreExecute() {
 			// TODO Auto-generated method stub
 			super.onPreExecute();
 			if(mLayoutBtn!=null)
 				mLayoutBtn.setVisibility(View.INVISIBLE);
-			
+
 			mProgress = new ElahProgress(SurveyCreateActivity.this);
 			mProgress.setCanceledOnTouchOutside(false);
 			mProgress.setCancelable(false);
 			if(mProgress!=null)
 				mProgress.show(R.string.msg_loading);
 		}
-		
+
 		@Override
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
@@ -396,7 +401,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 						mBtnCopySurvey.setVisibility(View.VISIBLE);
 					if(mBtnErrorCopying!=null)
 						mBtnErrorCopying.setVisibility(View.GONE);
-					
+
 				}
 				else if(result.equals(RESPONSE_SURVEY_EXPIRED)){
 					Util.showToast(mToast, R.string.msg_data_expired);
@@ -421,15 +426,15 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 					mBtnErrorCopying.setVisibility(View.VISIBLE);
 //				Util.showToast(mToast, R.string.error_server);
 			}
-			
+
 			if(mLayoutBtn!=null)
 				mLayoutBtn.setVisibility(View.VISIBLE);
 		}
-		
+
 		@Override
 		protected String doInBackground(String... params) {
 			String response = null;
-			
+
 			try {
 				JSONObject jsonResponse  = ElahHttpClient.executeGetAsJSON(Consts.SERVER_URL+"/sync?"+KEY_REQUEST+"="+VALUE_SURVEY_CHECK
 						+"&"+KEY_CUST_CODE+"="+params[0].replace(" ", "%20"));
@@ -441,19 +446,19 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		   	    	 		jsonResponse = ElahHttpClient.executeGetAsJSON(Consts.SERVER_URL+"/sync?"+KEY_REQUEST+"="+VALUE_SURVEY_CHECK
 									+"&"+KEY_CUST_CODE+"="+params[0].replace(" ", "%20"));
 		   	    	 		break;
-		   	    	 	
+
 		   	    	 	case FAILED:
 		   	    	 		return LOGIN_FAILED;
-		   	    	 	
+
 		   	    	 	case NO_PERMISSION:
 		   	    	 		return PERMISSION_NOT_ALLOWED;
-		   	    	 	
+
 		   	    	 	case SERVER_ERROR:
 		   	    	 		return LOGIN_SERVER_ERROR;
 		   	    	 }
-					
+
 				}
-				
+
 				response = jsonResponse.getString(KEY_RESPONSE);
 
 			}
@@ -472,9 +477,9 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			}
 			return response;
 		}
-		
+
 	}
-	
+
 	private class GetPreviousSurveyTask extends AsyncTask<String, Integer, Long>{
 
 		private static final long SERVER_ERROR = -101;
@@ -484,26 +489,26 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		private static final long LOGIN_FAILED = -112;
 		private static final long PERMISSION_NOT_ALLOWED = -113;
 		private static final long LOGIN_SERVER_ERROR = -114;
-		
+
 		private ElahProgress mProgress;
 		private Context 	   mContext;
-		
+
 		// ===========================================================
 		// Constructors
 		// ===========================================================
-		
+
 		private GetPreviousSurveyTask(Context context){
 			this.mContext = context;
 		}
-		
+
 		// ===========================================================
 		// Getter & Setter
 		// ===========================================================
-		
+
 		// ===========================================================
 		// Methods for/from SuperClass/Interfaces
 		// ===========================================================
-		
+
 		@Override
 		protected void onPreExecute() {
 			// TODO Auto-generated method stub
@@ -514,7 +519,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			if(mProgress!=null)
 				mProgress.show(R.string.msg_copying);
 		}
-		
+
 		@Override
 		protected void onPostExecute(Long result) {
 			// TODO Auto-generated method stub
@@ -558,7 +563,7 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 				finish();
 			}
 		}
-		
+
 		@Override
 		protected Long doInBackground(String... params) {
 			Long surveyId = SERVER_ERROR;
@@ -576,35 +581,35 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 		   							+KEY_REQUEST+"="+VALUE_SURVEY_GET_PREVIOUS
 		   							+"&"+KEY_CUST_CODE+"="+params[0].replace(" ", "%20")
 		   							+"&"+KEY_CURRENT_TIME+"="+Util.getCurrentTimeInMilliSeconds());
-		   	    	 		
+
 		   	    	 		break;
-		   	    	 	
+
 		   	    	 	case FAILED:
 		   	    	 		return LOGIN_FAILED;
-		   	    	 	
+
 		   	    	 	case NO_PERMISSION:
 		   	    	 		return PERMISSION_NOT_ALLOWED;
-		   	    	 	
+
 		   	    	 	case SERVER_ERROR:
 		   	    	 		return LOGIN_SERVER_ERROR;
 		   	    	 }
 				}
-				
+
 				JSONObject survey  = jsonResponse.getJSONObject("survey");
 				JSONArray surveyArray = survey.getJSONArray(TableSurveyAssortimento.KEY_ARRAY_NAME);
 				JSONArray competitorArray = survey.getJSONArray(TableSurveyCompetitor.KEY_ARRAY_NAME);
 				JSONArray promotionsArray = survey.getJSONArray(TableSurveyPromotions.KEY_ARRAY_NAME);
-				
+
 				//Prevent empty json from creating survey
 				if(surveyArray.length()==0&&competitorArray.length()==0&&promotionsArray.length()==0)
 					return SERVER_ERROR;
-				
+
 				surveyId = TableSurvey.createNewSurvey(new Survey(salesPersonCode, customerCode,TableSurvey.FLAG_FROM_SERVER, Util.getCurrentDateTime(),1,0));
-				
+
 				TableSurveyAssortimento.create(surveyId,salesPersonCode,survey.getJSONArray(TableSurveyAssortimento.KEY_ARRAY_NAME));
 				TableSurveyCompetitor.create(surveyId,salesPersonCode,survey.getJSONArray(TableSurveyCompetitor.KEY_ARRAY_NAME));
 				TableSurveyPromotions.create(surveyId,salesPersonCode, survey.getJSONArray(TableSurveyPromotions.KEY_ARRAY_NAME));
-				
+
 				return surveyId;
 			} catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
@@ -623,10 +628,10 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 				e.printStackTrace();
 				surveyId = JSON_EXCEPTION;
 			}
-			
+
 			return surveyId;
-		
-		
+
+
 		}
 	}
 	private class NetConnectionReceiver extends BroadcastReceiver{
@@ -636,11 +641,11 @@ public class SurveyCreateActivity extends Activity implements OnClickListener{
 			if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)){
 				setUpCopyButton();
 			}
-			
+
 		}
 
 
-		
+
 	}
 
 
